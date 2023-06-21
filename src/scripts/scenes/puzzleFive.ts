93a42496d91f76c3da77dc6f839ce068b693bc2d
Marco <marco.demelo2@slc.student.on.ca>
2023-06-21 14:28:59 -0400

    Puzzle 5 99% done need 1 fix

--- src/scripts/scenes/puzzleFive.ts
+++ src/scripts/scenes/puzzleFive.ts
@@ -1,76 +1,129 @@
 export default class Game extends Phaser.Scene {
-    constructor() {
-      super({ key: 'puzzleFive' });
-    }
-  
-    create() {
-        //Make it so the verification actually works, probably change how it works. \
-        //Idea 1: Make it so there's boxes specifically where they can go but u can drag it wherever, whatever image
-        // is in the box at the time will be replaced by the image just picked up and it'll go in that one's box
-        var Return = this.add.image(95, 40, 'Return').setInteractive().on('pointerdown', ()=> {
-            this.scene.start('puzzle5-6');//This is meant to change pages
-    
-          });
-      const table = this.add.image(400, 480, 'table');
-      const key5 = this.add.image(783, 500, 'Key5');
-      key5.setAlpha(0);
-  
-      const bottleA = this.add.image(315, 300, 'bottleA').setInteractive({ draggable: true });
-      const bottleB = this.add.image(425, 300, 'bottleB').setInteractive({ draggable: true });
-      const bottleC = this.add.image(535, 300, 'bottleC').setInteractive({ draggable: true });
-  
-      bottleA.setScale(0.16);
-      bottleB.setScale(0.16);
-      bottleC.setScale(0.16);
-  
-      const tableBounds = table.getBounds();
-  
-      const correctOrder = ['C', 'A', 'B'];
-      const droppedBottles = {};
-  
-      const checkOrder = () => {
-        const currentOrder = Object.keys(droppedBottles).sort((a, b) => {
-          const indexA = correctOrder.indexOf(a);
-          const indexB = correctOrder.indexOf(b);
-          return indexA - indexB;
-        });
-  
-        if (currentOrder.join('') === correctOrder.join('')) {
-          console.log('Unlock the next stage!');
-          key5.setAlpha(1);
-        } else {
-          console.log('Incorrect order, try again!');
-        }
-      };
-  
-      bottleA.setData('key', 'A');
-      bottleB.setData('key', 'B');
-      bottleC.setData('key', 'C');
-  
-      this.input.on('dragstart', (pointer, gameObject) => {
-        gameObject.setTint(0xaaaaaa);
-      });
-  
-      this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
-        const minX = tableBounds.left + 50;
-        const maxX = tableBounds.right - 50;
-  
-        gameObject.x = Phaser.Math.Clamp(dragX, minX, maxX);
-        gameObject.y = dragY;
-      });
-  
-      this.input.on('dragend', (pointer, gameObject) => {
-        gameObject.clearTint();
-        gameObject.x = Math.floor(gameObject.x / 100) * 100 + 50;
-        gameObject.y = 300;
-  
-        if (gameObject.input.dragged && !gameObject.input.target) {
-          delete droppedBottles[gameObject.getData('key')];
-        } else {
-          droppedBottles[gameObject.getData('key')] = gameObject;
-          checkOrder();
-        }
-      });
-    }
+  constructor() {
+    super({ key: 'puzzleFive' });
   }
-  
+
+  create() {
+    var Return = this.add
+      .image(95, 40, 'Return')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.scene.start('puzzle5-6'); // This is meant to change pages
+      });
+
+    const table = this.add.image(400, 480, 'table');
+    const key5 = this.add.image(783, 500, 'Key5');
+    key5.setScale(0.5);
+    key5.setAlpha(0);
+
+    const createBox = (x: number, y: number) => {
+      return this.add
+        .rectangle(x, y, 100, 100, 0xffffff)
+        .setStrokeStyle(2, 0x000000)
+        .setInteractive()
+        .setData('id', null) as Phaser.GameObjects.Rectangle & { id: string | null };
+    };
+
+    const box1 = createBox(250, 340);
+    const box2 = createBox(400, 340);
+    const box3 = createBox(550, 340);
+
+    const boxes = [box1, box2, box3];
+    const droppedBottles: (Phaser.GameObjects.Image | undefined)[] = [undefined, undefined, undefined];
+
+    const updateConsoleLogs = () => {
+      for (let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
+        const droppedBottle = droppedBottles[i];
+
+        if (droppedBottle) {
+          console.log(`Box${i + 1} has ID: ${droppedBottle.getData('id')}`);
+        }
+      }
+    };
+
+    const checkOrder = () => {
+      if (droppedBottles.some((bottle) => !bottle)) {
+        return; // Exit if not all boxes have a bottle
+      }
+
+      const currentOrder = droppedBottles.map((bottle) => bottle?.getData('id')).join('');
+      const correctOrder = 'CAB';
+
+      if (currentOrder === correctOrder) {
+        console.log('Unlock the next stage!');
+        key5.setAlpha(1);
+      } else {
+        console.log('Incorrect order, try again!');
+      }
+    };
+
+    const bottleA = this.add.image(150, 450, 'bottleA').setScale(0.16).setData('id', 'A');
+    const bottleB = this.add.image(300, 450, 'bottleB').setScale(0.16).setData('id', 'B');
+    const bottleC = this.add.image(450, 450, 'bottleC').setScale(0.16).setData('id', 'C');
+
+    const bottles = [bottleA, bottleB, bottleC];
+
+    bottles.forEach((bottle, index) => {
+      bottle.setInteractive({ draggable: true });
+      this.input.setDraggable(bottle);
+
+      bottle.on('dragstart', () => {
+        this.children.bringToTop(bottle);
+      });
+
+      bottle.on('drag', (pointer, dragX, dragY) => {
+        bottle.setPosition(dragX, dragY);
+      });
+
+      bottle.on('dragend', (pointer) => {
+        let droppedInBox = false;
+      
+        boxes.forEach((box, boxIndex) => {
+          const boxBounds = box.getBounds();
+          const bottleBounds = bottle.getBounds();
+      
+          if (Phaser.Geom.Rectangle.Contains(boxBounds, bottleBounds.centerX, bottleBounds.centerY)) {
+            const droppedBottle = droppedBottles[boxIndex];
+      
+            if (droppedBottle && droppedBottle !== bottle) {
+              const originalPosition = this.getOriginalBottlePosition(bottles.indexOf(droppedBottle));
+              droppedBottle.setPosition(originalPosition.x, originalPosition.y);
+              const droppedBottleIndex = droppedBottles.indexOf(droppedBottle);
+              droppedBottles[droppedBottleIndex] = undefined;
+            }
+      
+            bottle.setPosition(box.x, box.y);
+            droppedBottles[boxIndex] = bottle;
+            box.setData('id', bottle.getData('id')); // Assign ID to the box
+            droppedInBox = true;
+          }
+        });
+      
+        if (!droppedInBox) {
+          const originalPosition = this.getOriginalBottlePosition(index);
+          bottle.setPosition(originalPosition.x, originalPosition.y);
+          const boxIndex = droppedBottles.indexOf(bottle);
+          droppedBottles[boxIndex] = undefined;
+          box1.setData('id', null); // Reset ID of the box
+          box2.setData('id', null);
+          box3.setData('id', null);
+        }
+      
+        updateConsoleLogs();
+        checkOrder();
+      });
+    });
+  }
+
+  getOriginalBottlePosition(index: number) {
+    const startX = 150;
+    const spacingX = 150;
+    const startY = 450;
+    const bottleWidth = 64;
+    const offsetX = bottleWidth / 2;
+    const x = startX + index * spacingX + offsetX;
+    const y = startY;
+    return new Phaser.Math.Vector2(x, y);
+  }
+}
